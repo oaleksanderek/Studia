32140a5826b25348c306b84dbb75c4638cdfaeff
oaleksanderek <117563832+oaleksanderek@users.noreply.github.com>
2022-11-27 11:46:19 +0100

    Add files via upload

---
+++ LAB3/lab5_6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int przekroj(int a1,int a2,int b1,int b2)
+{
+    if(a1<a2 && b1>a2)
+    {
+        return b1;
+    }
+    else if(a1<a2 && b1<a2)
+    {
+        return -1;
+    }
+    else if(a2<a1 && b1<b2)
+    {
+        return b1;
+    }
+}
+
+int main()
+{
+    int a1,b1,a2,b2;
+
+    printf("Podaj dwie liczby naturalne a1,b1: ");
+    scanf("%i %i",&a1,&b1);
+    printf("Podaj dwie liczby naturalne a2,b2: ");
+    scanf("%i %i",&a2,&b2);
+
+    printf("%i",przekroj(a1,a2,b1,b2));
+}

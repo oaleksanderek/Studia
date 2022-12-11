130831008c890d9381b944f73bd7e8a40fdb965e
oaleksanderek <117563832+oaleksanderek@users.noreply.github.com>
2022-12-11 10:04:22 +0100

    Create lab6_4.c

---
+++ LAB4/lab6_4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fibb(int n){
+   if(n==0)
+   {
+       return 0;
+   }
+   else if(n==1)
+   {
+       return 1;
+   }
+   else if(n>1)
+   {
+       return fibb(n-1)+fibb(n-2);
+   }
+}
+
+int main()
+{
+    int n;
+    printf("Podaj liczbe n: ");
+    scanf("%i",&n);
+    
+    printf("%i",fibb(n));
+
+    return 0;
+}

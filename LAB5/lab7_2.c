0d477a948a859f76439dbb48f8a51f831c86cd96
oaleksanderek <117563832+oaleksanderek@users.noreply.github.com>
2022-12-18 09:32:49 +0100

    Update lab7_2.c

--- LAB5/lab7_2.c
+++ LAB5/lab7_2.c
@@ -4,18 +4,16 @@
 int main()
 {
     int tab[10];
-    int *wsk;
     for(int i=0;i<10;i++)
     {
         printf("Podaj liczby do tablicy: ");
-        scanf("%i",&tab[i]);
+        scanf("%d",&*(tab+i));
     }
     system("clear");
     printf("Tablica za pomoca wskaznikow:");
     for(int i=0;i<10;i++)
     {
-        wsk=&tab[i];
-        printf("%i\t",*wsk);
+        printf("%d\t",*&*(tab+i));
     }
 
     return 0;

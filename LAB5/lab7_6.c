48c0aaaa7aa45cf59a40d87ada9058cae35e9187
oaleksanderek <117563832+oaleksanderek@users.noreply.github.com>
2022-12-18 11:54:46 +0100

    do poprawki, działa do n=6

---
+++ LAB5/lab7_6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void funkcja(int* wskaz1,int* wskaz2)
+{
+    int dlugosc=wskaz2-wskaz1;
+    
+    for(int i=1;i<=dlugosc;i++)
+    {
+        wskaz2[dlugosc-i]=i;
+    }
+    for(int i=dlugosc-1;i>=0;i--)
+    {
+        printf("%lld\t",wskaz2[i]);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Podaj n: ");
+    scanf("%i",&n);
+    int tab[n];
+    int* wsk1=tab;
+    int* wsk2=&tab[n];
+
+    funkcja(wsk1,wsk2);
+
+    return 0;
+}
